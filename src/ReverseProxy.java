a013e1d1ea7fc54e18df49e1773f78442884dfa9
João Pires Barreira <jpiresbarreira@gmail.com>
2018-05-22 00:24:29 +0100

    Implementação do reverse proxy e comunicação com clientes

--- src/ReverseProxy.java
+++ src/ReverseProxy.java
@@ -1,36 +1,103 @@
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ReverseProxy {
 
     private static final int RP_PORT = 80;
+    private static final int BUFFER_SIZE = 1024;
+
+    private static class BackendHandler extends Thread {
+        private final Socket clientSocket;
+        private final Socket backendSocket;
+
+        private BackendHandler(Socket clientSocket, Socket backendSocket) {
+            this.clientSocket = clientSocket;
+            this.backendSocket = backendSocket;
+        }
+
+        public void run() {
+            try {
+                int nRead;
+                final byte[] buffer = new byte[BUFFER_SIZE];
+
+                InputStream fromBackend = backendSocket.getInputStream();
+                OutputStream toClient = clientSocket.getOutputStream();
+
+                while ((nRead = fromBackend.read(buffer)) != -1) {
+                    toClient.write(buffer, 0, nRead);
+                }
+            }
+            catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private static class ClientHandler extends Thread {
+
+        private Socket clientSocket;
+        private ServerStatus backendServer;
+
+        private ClientHandler(Socket clientSocket, ServerStatus backendServer) {
+            this.clientSocket = clientSocket;
+            this.backendServer = backendServer;
+        }
+
+        public void run() {
+            try {
+                if (backendServer == null) {
+                    PrintWriter pw = new PrintWriter(clientSocket.getOutputStream());
+
+                    pw.write("HTTP/1.1 404 Not Found\n");
+                    pw.flush();
+
+                    clientSocket.close();
+                }
+                else {
+                    Socket backendSocket = new Socket(backendServer.getIP(), RP_PORT);
+
+                    InputStream fromClient = clientSocket.getInputStream();
+                    OutputStream toBackend = backendSocket.getOutputStream();
+
+                    int nRead;
+                    byte[] buffer = new byte[BUFFER_SIZE];
+
+                    BackendHandler backendHandler = new BackendHandler(clientSocket, backendSocket);
+                    backendHandler.start();
+
+                    while ((nRead = fromClient.read(buffer)) != -1) {
+                        toBackend.write(buffer, 0, nRead);
+                    }
+                    backendHandler.join();
+
+                    clientSocket.close();
+                    backendSocket.close();
+                }
+            }
+            catch (IOException | InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 
     public static void main(String[] args) throws IOException {
-
-        ServerSocket ss = new ServerSocket(RP_PORT);
-
         StatusTable table = new StatusTable();
 
         UDPMonitor monitor = new UDPMonitor(table);
         monitor.start();
 
-        while (true) {
-            Socket clientSocket = ss.accept();
+        ServerSocket serverSocket = new ServerSocket(RP_PORT);
+        Socket clientSocket = null;
 
-            ServerStatus server = table.chooseServer();
+        while ((clientSocket = serverSocket.accept()) != null) {
+            ServerStatus backendServer = table.chooseServer();
 
-            if (server == null) {
-                // informar cliente que não existem servidores e terminar conexão
-
-                System.out.println("ERROR: No available servers");
-                break;
-            }
-
-            Socket serverSocket = new Socket(server.getIP(), server.getPort());
-
-            ConnectionThread ct = new ConnectionThread(clientSocket, serverSocket);
-            ct.start();
+            ClientHandler clientHandler = new ClientHandler(clientSocket, backendServer);
+            clientHandler.start();
         }
     }
 }
